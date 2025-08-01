8d313b2f812c511c1be35848f08a458ee59b8917
Vanshika Prakash <vanshikaprakash05@gmail.com>
2025-08-01 21:17:40 +0530

    button refined

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { FiCopy } from 'react-icons/fi';
+
 class App extends React.Component {
-    state = { advice: ''};
+  state = {
+    advice: 'Want Random Gyaan?',
+    loading: false,
+    copied: false
+  };
 
-    componentDidMount() {
-       
-    }
+  componentDidMount() {
+    this.fetchAdvice();
+  }
 
-    fetchAdvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-            .then(response => {
-                const { advice } = response.data.slip;
-                this.setState({ advice: advice });
-            })
-            .catch(error => {
-                console.error("Error fetching advice:", error);
-            });
-    }
+  fetchAdvice = () => {
+    this.setState({ loading: true, advice: 'Fetching Random Gyaan for you :)', copied: false });
 
-    render() {
-        const { advice } = this.state;
+    axios.get('https://api.adviceslip.com/advice')
+      .then(response => {
+        const { advice } = response.data.slip;
+        this.setState({ advice, loading: false });
+      })
+      .catch(error => {
+        console.error("Error fetching advice:", error);
+        this.setState({ advice: 'Failed to fetch advice. Try again!', loading: false });
+      });
+  }
 
-        return (
-            <div className="app">
-                <div className="card">
-                    <h1 className="heading">{advice}</h1>
-                </div>
-                <button className="button" onClick={this.fetchAdvice}>
-                    <span>Get Advice</span>
-                </button>
-            </div>
-        );
+  handleCopy = () => {
+    navigator.clipboard.writeText(this.state.advice).then(() => {
+      this.setState({ copied: true });
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
 
-    }
+  render() {
+    const { advice, loading, copied } = this.state;
+
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <div className="copy-icon" onClick={this.handleCopy} title="Copy Advice">
+            <FiCopy size={20} />
+            {copied && <span className="copied-msg">Copied!</span>}
+          </div>
+        </div>
+
+        <button className="button" onClick={this.fetchAdvice} disabled={loading}>
+          <span><strong>Get Advice</strong></span>
+        </button>
+      </div>
+    );
+  }
 }
+
 export default App;
+
+
